6522614f0e9aa95ed93889f938b5006c80b4da01
ely-hi <135531388+ely-hi@users.noreply.github.com>
2025-02-07 17:47:20 -0500

    completed movement, spawning, and destruction of some game object (the bullet)

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -13,7 +13,8 @@ public class PlayerMovementScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public GameObject shootPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            GameObject clone = Instantiate(prefab);
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
+        }
+    }
+}
